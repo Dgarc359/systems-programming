795d2072cd7cb7c88abf0f45dee96ca70744a4a8
Dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2022-08-26 20:04:42 -0400

    Update print-example.c
    decimal to octal and hexadecimal conversion notes

--- 8-25/print-example.c
+++ 8-25/print-example.c
@@ -2,18 +2,22 @@
 
 void main(void) {
   /**
-   * octal:
-   * 20 % 8 = 4
-   * 20 / 8 = 2
-   * 2 % 8 = 2
-   * 20, 24 
+   * https://www.youtube.com/watch?v=ayul1fmZd0Y
+   * decimal (base 10) -> octal (base 8): 
    * 
-   * hex:
-   * 20 % 10 = 4
-   * 20 / 16 = 1
-   * 1 % 16 - 1
+   * 20 / 8 = 2R[4] || 20 % 8 = [4] LEAST SIGNIFICANT
+   * Take result of division and modulus by 8, if it's smaller than 8, remainder is the original number: Proof ( 2 / 8 = 0.25 | 0.25 * 8 = 2 )
+   * 2 % 8 = [2] MOST SIGNIFICANT
+   * Order by Most -> Least Significant: 24 
    * 
-   * 20, 24, 14
+   *
+   * https://www.youtube.com/watch?v=4um7-QbpAQ8
+   * decimal (base 10) -> hexadecimal (base 16):
+   * 20 / 16 = 1R[4] || 20 % 16 = [4] LEAST SIGNIFICANT
+   * Same as above, take result of division, modulus by 16 this time.
+   * 1 % 16 = [1] MOST SIGNIFICANT
+   * 
+   * 14
    */
   printf("%d, %o, %x\n", 20, 20, 20);
 
