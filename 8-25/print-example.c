6b4307fca9985fbf9e8c1c89c7beb3a87099bf4e
Dgarc359 <david.garcia@revature.net>
2022-08-26 21:02:02 -0400

    print-example notes

--- 8-25/print-example.c
+++ 8-25/print-example.c
@@ -19,40 +19,44 @@ void main(void) {
    * 
    * 14
    */
-  printf("%d, %o, %x\n", 20, 20, 20);
+  printf("%d, %o, %x\n", 20, 20, 20); // prints 20, 24, 14
 
   /**
    * hexadecimal -> decimal: (NOTE: 0x is just the identifier for hex values in C, for our purposes, we can ignore it in calculations)
    * 2 * 16 = 32
    * 0 * 1 = 0
-   * 32 + 0 = 32
+   * 32 + 0 = [32]
    * OR
    * convert each digit to binary first: (SIMPLER)
    * 20 = 0_0_1_0 | 0_0_0_0
-   * combined = 0010 0000 = 32
+   * combined = 0010 0000 = [32]
    * 
    * octal:
    * 1: convert 20 to binary
    *  0010 0000
    * 2: convert binary to octal (every 3 bits, is one digit in octal)
    * 00 100 000
-   * 0    4   0 = 40
+   * 0    4   0 = [40]
    * 
-   * hex: ??
+   * hex: 0x20 (0x is ignored) = [20] 
    */
-  printf("%d, %o, %x\n", 0X20, 0X20, 0X20);
+  printf("%d, %o, %x\n", 0X20, 0X20, 0X20); // prints 32, 40, 20
 
   /**
-   * decimal: ???
-   * 
+   * https://www.youtube.com/watch?v=YCM2JReWS10
+   * octal -> decimal:
    * octal:
    *  20 ->
-   *    binary 010 000 -> ????
+   *    octal binary (groups of 3) 010 000 ->
+   *        decimal [16]
    * 
-   * hex: 
+   * octal -> octal: ignore leading 0 = [20]
+   *
+   * octal -> hex: 
    * 20 ->
-   *    binary 010 000 -> ????
-   *      hex 10
+   *    octal binary (groups of 3) 010 000 ->
+   *      hex binary (groups of 4) 0001 0000 ->
+   *        decimal [10]
    */
-  printf("%d, %o, %x\n", 020, 020, 020);
+  printf("%d, %o, %x\n", 020, 020, 020); // prints 16, 20, 10
 }
