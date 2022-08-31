826803148d1b8d783f72cfa40839e40bc091207a
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2022-08-30 20:21:34 -0400

    times up

---
+++ class-notes/8-30/README.md
@@ -0,0 +1,7 @@
+# 8-30 Notes
+
+#### Char Arrays
+strings are stored as an array of characters 
+"hello" = ["h", "e", "l", "l", "o", "\n", "\0"] OR each char's ascii code
+   
+"\0" is used to determine when the string ends, which informs us what the length of the array is
---
+++ class-notes/8-30/in-class-assignment.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int flipChars(int x);
+void reverse(char str[]);
+
+main () {
+  char temp;
+  /**
+   * press ctrl + d to close buffer without inputting newline ('\n') 
+   */
+  while(1){
+    temp = getchar(); // until you press enter, data is in buffer stream
+    if ( temp == ' ' 
+      || temp == '\t' 
+      || temp == '\n' 
+      || temp == EOF
+    )
+      break;
+    
+    // putchar(temp);
+    temp = flipChars(temp);
+    putchar(temp);
+  }
+
+  reverse("test");
+
+}
+
+int flipChars(int c) {
+  if (c >= 'A' && c <= 'Z')
+    return c + 'a' - 'A';
+  else
+    return c - 'a' + 'A';
+}
+
+void reverse(char str[]) {
+  char reversed[0] = "";
+  for (int i = 0; str[i] == '\0'; i++) {
+    char *strcat(reversed, i);
+  }
+  printf("%s", reversed);
+}
+
+void remove_second_half(char str[]) {
+
+}
