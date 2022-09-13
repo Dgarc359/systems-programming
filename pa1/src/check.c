dbbb518f7300d76dd0e0202948521b282e47492f
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2022-09-13 19:48:15 -0400

    init

---
+++ pa1/src/check.c
@@ -0,0 +1,7 @@
+int check(char str[]) {
+  // returns 1 if input stream contains a balanced string
+  // 0 otherwise
+
+  // use most recent list of pairs
+  // use a stack to check the balance of opening and closing symbols
+}
