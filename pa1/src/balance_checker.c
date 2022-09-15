a507af27acc7930b3ae1cc7fade505c7f7b01107
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2022-09-15 19:16:07 -0400

    basic structure completed

--- pa1/src/balance_checker.c
+++ pa1/src/balance_checker.c
@@ -1,31 +1,73 @@
 #include <stdio.h>
-#include <balance.h>
-
-char get_word(char command[], int max_length) {
+char get_word(char command[], int max_length)
+{
   char last_character;
   int i;
-  for (i=0; i<max_length -1 && (command[i] = getchar()) != ' ' && command[i] !='\n'; i++)
+  for (i = 0; i < max_length - 1 && (command[i] = getchar()) != ' ' && command[i] != '\n'; i++)
+    ;
   last_character = command[i];
-  command[i] = '\0';
+  command[i] = '\0'; // end of string sign
   return last_character;
 }
-
-int main() {
+char get_line(char command[], int max_length)
+{
+  char last_character;
+  int i;
+  for (i = 0; i < max_length - 1 && (command[i] = getchar()) != '\n'; i++)
+    ;
+  last_character = command[i];
+  command[i] = '\0'; // end of string sign
+  return last_character;
+}
+int equals(char str1[], char str2[])
+{
+  int i = 0;
+  while (1)
+  {
+    if (str1[i] == '\0' && str2[i] == '\0')
+      return 1;
+    if (str1[i] == '\0' || str2[i] == '\0')
+      return 0;
+    if (str1[i] != str2[i])
+      return 0;
+    i++;
+  }
+}
+int main()
+{
   char command[10];
   char str[400];
   char next_char;
-
-  while(1) {
+  while (1)
+  {
     next_char = get_word(command, 10);
-    if(compare(command, "quit")) {
+    if (equals(command, "quit"))
+    {
       break;
-    } else if (compare(command, "check")) {
-      if(next_char == '\n')
+    }
+    else if (equals(command, "check"))
+    {
+      if (next_char == '\n')
         printf("ERROR! Try again!\n");
-      else {
+      else
+      {
         get_line(str, 400);
         printf("Checking %s...\n", str);
       }
+    }
+    else if (equals(command, "pair"))
+    {
+      if (next_char == '\n')
+        printf("ERROR! Try again!\n");
+      else
+      {
+        get_line(str, 400);
+        printf("Pairing %s...\n", str);
+      }
+    }
+    else
+    {
+      printf("Invalid command! Try again!\n");
     }
   }
 }
