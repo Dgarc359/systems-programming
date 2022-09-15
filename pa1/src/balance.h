ee816f4dea9f488250cb89b1f84d03bf772fe02d
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2022-09-15 19:37:08 -0400

    add work before we go to class

--- pa1/src/balance.h
+++ pa1/src/balance.h
@@ -1,5 +1,12 @@
+#include <stdio.h>
 int check(char str[]);
 void pair(char list[]);
 
 int opening_symbols[51];
 int closing_symbols[51];
+
+int *opening_symbols_pointer[51];
+opening_symbols_pointer = &opening_symbols;
+
+int *closing_symbols_pointer[51];
+closing_symbols_pointer = &closing_symbols;
--- pa1/src/check.c
+++ pa1/src/check.c
@@ -1,6 +1,14 @@
 #include "balance.h"
 
 int check(char str[]) {
+  int length = 0;
+  int check;
+  for(int i = 0; check != NULL; check = opening_symbols_pointer[i], i++, length++);
+  if(length == 0) {
+    // use parenthesis to check balance
+  } else {
+    // length is not 0 so user has defined their own pairs
+  }
   // returns 1 if input stream contains a balanced string
   // 0 otherwise
 
