d1c296c205ca50f7747641e668be0aeb0285de7e
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2022-09-21 19:30:12 -0400

    add work so far

--- pa1/src/pair.c
+++ pa1/src/pair.c
@@ -4,39 +4,36 @@ int closing_symbols[51];
 
 void pair(char list[])
 {
-  // printf("test\n");
   printf("test: %s\n", list);
   int opening_symbols_counter = 0;
+  int closing_symbols_counter = 0;
+  
+  for (int i = 0; i < 52; i++) {
+    opening_symbols[i] = NULL;
+    closing_symbols[i] = NULL;
+  }
+
   for (int i = 0; i < 401 && list[i] != '\0'; i++)
   { // 400 is our max string size
-    // printf("%i: char %c\n", i, list[i]);
-    if (list[i] == ' ')
+    if (list[i] == ' ' || list[i] == '\n' || list[i] == '\t')
     {
-      // Go to the next iteration
       continue;
     }
     else if (list[i] == ',')
     {
       // this means the next value will be our closing symbol
       // take the next value and increase i so that it increases by two and skips
-      i++;
-    }
-    else
-    {
-      // opening symbol, put it opening symbol array
-      opening_symbols[opening_symbols_counter] = list[i];
-      opening_symbols_counter++;
+      opening_symbols[opening_symbols_counter++] = list[i - 1];
+      closing_symbols[closing_symbols_counter++] = list[i + 1];
     }
     if (i == 400) {
       opening_symbols[opening_symbols_counter] = NULL;
     }
   }
 
-  for (int i = 0; i < 52 && opening_symbols[i] != NULL; i++)
+  for (int i = 0; i < 52 && opening_symbols[i] != NULL && closing_symbols[i] != NULL; i++)
   {
-    printf("opening symbol: %i\n", opening_symbols[i]);
+    printf("opening symbol: %c\n", opening_symbols[i]);
+    printf("closing symbols: %c\n", closing_symbols[i]);
   }
-  // gets list of opening and closing symbols
-
-  // stores opening / closing in arrays
 }
