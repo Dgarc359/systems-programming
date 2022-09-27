2c9f7ebb1eaff363f3e66c3ef7db2bdff168b934
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2022-09-27 11:37:02 -0400

    stuff so far

--- pa1/src/check.c
+++ pa1/src/check.c
@@ -1,51 +1,73 @@
 #include "balance.h"
+#define BUFSIZE 400
 
-int isNotClosingSymbol(int symbol) {
-  for (int i = 0; i != NULL; i++)
-    if (closing_symbols[i] == symbol) {
-      printf("char from closing_symbols: %c\n", closing_symbols[i]);
-      printf("char is closing symbol in check fn\n");
-      return 0;
-    }
-  return 1;
-}
-
-int isNotOpeningSymbol(int symbol) {
-  for (int i = 0; i != NULL; i++) {
-    printf('%c\n', opening_symbols[i]);
-    if (opening_symbols[i] == symbol){
-      printf("char is opening symbol in check fn\n");
+int isNotClosingSymbol(char symbol) {
+  for (int i = 0; closing_symbols[i] != '\0'; i++) {
+    char check_symbol = closing_symbols[i];
+    if (check_symbol == symbol) {
       return 0;
     }
   }
   return 1;
+}
+
+int isNotOpeningSymbol(char symbol) {
+  for (int i = 0; opening_symbols[i] != '\0'; i++) {
+    char check_symbol = opening_symbols[i];
+    if (check_symbol == symbol){
+      return 0;
+    }
+  }
+  return 1;
+}
+
+checkClosingSymbolBalances(char peeked, char check_char) {
+    for (int i = 0; opening_symbols[i] != '\0'; i++)
+        if (opening_symbols[i] == peeked && closing_symbols[i] == check_char)
+            return 1;
+        return 0;
 }
 
 int check(char str[]) {
+
+  char buf[BUFSIZE];
+  char* stack = buf;
+
+  int bufp = 0;
+  int* stackPointer = &bufp;
+
+  for(int i = 0; i <= BUFSIZE; i++)
+      buf[i] = '\0';
   printf("open: %c close: %c\n", opening_symbols[0], closing_symbols[0]);
 
   int length = 0;
-  int check_char;
-  getchar();
-  // for (int i = 0, check_char = str[i]; check_char != NULL || check_char != EOF; i++){
-  for (int i=0; str[i] != '\0'; i++) {
+  char check_char;
+  for (int i = 0; str[i] != '\0'; i++)
+    length++;
+  
+  for (int i=0; i <= length ; i++) {
     check_char = str[i];
     printf("inside for loop\ncheck_char: %c\n", str[i]);
-    if(isNotClosingSymbol(check_char) && !isNotOpeningSymbol(check_char)) { //char is opening symbol
+
+    int isNotClosingSymbolReturn = isNotClosingSymbol(check_char);
+    int isNotOpeningSymbolReturn = isNotOpeningSymbol(check_char);
+    if(isNotClosingSymbolReturn && !isNotOpeningSymbolReturn) { //char is opening symbol
       printf("char is opening symbol\n");
-      push(check_char);
-    } else if (!isNotClosingSymbol(check_char) && isNotOpeningSymbol(check_char)) { // char is closing symbol
-      printf("char is opening symbol\n");
-      int peeked = peek();
-      if (peeked == check_char)
-        pop();
+      push(check_char, buf, stackPointer);
+      printf("pushed elem");
+    } else if (!isNotClosingSymbolReturn && isNotOpeningSymbolReturn) { // char is closing symbol
+      printf("char is closing symbol\n");
+      char peeked = peek(buf, stackPointer);
+      int closingSymbolBalancesOpeningSymbol = checkClosingSymbolBalances(peeked, check_char);
+      if (closingSymbolBalancesOpeningSymbol)
+        pop(stack, stackPointer);
     } else {
-      printf("char is neither");
+      printf("char is neither\n");
     }
-    getchar();
   }
 
-  if (peek() == NULL) {
+  char peeked = peek(buf, bufp);
+  if (peeked == '\0') {
     printf("balanced");
     return 1;
   }
@@ -56,6 +78,28 @@ int check(char str[]) {
 
   // use most recent list of pairs
   // use a stack to check the balance of opening and closing symbols
-  printf('unbalanced');
+  printf("unbalanced");
   return 0;
+}
+
+int push(int element, char stack[], int* pointer){
+    if (*pointer >= BUFSIZE) {
+        printf("stack is at max limit");
+        return 1;
+    }
+    else {
+        stack[(*pointer)++] = element;
+        return 0;
+    }
+}
+
+void pop(char stack[], int* pointer) {
+    if(*pointer > 0)
+        stack[--(*pointer)] = '\0';
+}
+
+char peek(char stack[], int* pointer) {
+    int i;
+    for (i = 0; stack[i] != '\0' ; i++);
+    return stack[--i];
 }
